0a388cf863e0e51ff23045d29614f76376f899ce
Luke O'Sullivan <osulluke@gmail.com>
2016-10-30 13:27:56 -0700

    Update HelloWorld
    There are a couple differences, I've found, with the methods used in the class example vs. the one I made that required a couple of extra imports. One was the java.awt.* library, as well as a 'Font' constructor in order to actually build a font.

--- Assignment1/HelloWorld.java
+++ Assignment1/HelloWorld.java
@@ -1,8 +1,16 @@
 import acm.program.*;
 import acm.graphics.*;
+import java.awt.*;
+
 
 public class HelloWorld extends GraphicsProgram {
 	public void run() {
 		add (new GLabel ("Hello, world!", 100, 75));
+		add (new GOval (50,50,30,30));
+		GLabel label = new GLabel("hello, world", 200, 150);
+		Font myFont = new Font("SansSerif", Font.BOLD, 36);
+		label.setFont(myFont);
+		label.setColor(Color.RED);
+		add(label);
 	}
 }
