7f48faee621178570a7999ff90a202d38b8e5de1
Luke O'Sullivan <osulluke@gmail.com>
2016-11-06 21:58:22 -0700

    Problem 4 strategy.
    Update the problem 4 strategy with a couple helper functions and the overall strategy for solving the problem.

--- Assignment1/Problem4.java
+++ Assignment1/Problem4.java
@@ -2,8 +2,29 @@ import stanford.karel.*;
 
 public class Problem4 extends SuperKarel {
 	public void run() {
-		
+		placeBrickInMiddle();
+		return;
 	}
 	
+	public void placeBrickInMiddle() {
+		/*
+		 * Problem strategy will be to lay down one full row of bricks
+		 * and then cycle back and forth between the end points, picking
+		 * up the ends of the rows, in turn, until there is only one 
+		 * brick remaining.
+		 */
+		layDownFirstRow();
+		pickUpEnds();
+		
+		return;
+	}
+	
+	public void layDownFirstRow() {
+		return;
+	}
+	
+	public void pickUpEnds() {
+		return;
+	}
 	
 }
