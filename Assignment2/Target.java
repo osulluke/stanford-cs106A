12701e2f789f1e4382cd20a02c2c2dcec842d33c
Luke O'Sullivan <osulluke@gmail.com>
2016-11-24 15:58:45 -0700

    Target problem.
    Here, I've got all the code put together, but for some reason, it doesn't want to display any of the rings...hmmm, maybe what's needed is a setFilled call...

--- Assignment2/Target.java
+++ Assignment2/Target.java
@@ -1,7 +1,7 @@
 /*
  * File: Target.java
- * Name: 
- * Section Leader: 
+ * Name:
+ * Section Leader:
  * -----------------
  * This file is the starter file for the Target problem.
  */
@@ -10,8 +10,31 @@ import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
 
-public class Target extends GraphicsProgram {	
+public class Target extends GraphicsProgram {
 	public void run() {
-		/* You fill this in. */
+		GCanvas canv = new GCanvas();
+		int height = canv.getHeight();
+		int width  = canv.getWidth();
+
+		int oSize = 72;
+		int mSize = (int) .65 * oSize;
+		int cSize = (int) .30 * oSize;
+
+		GOval outer = new GOval(oSize, oSize, width / 2, height / 2);
+		GOval middle = new GOval(mSize, mSize, width / 2, height / 2);
+		GOval center = new GOval(cSize, cSize, width / 2, height / 2);
+		
+		outer.setColor(Color.red);
+		middle.setColor(Color.white);
+		center.setColor(Color.red);
+/*
+		add(outer.setColor(Color.RED));
+		add(middle.setColor(Color.RED));
+		add(center.setColor(Color.RED));
+*/
+		canv.add(outer);
+		canv.add(middle);
+		canv.add(center);
+		return;
 	}
 }
