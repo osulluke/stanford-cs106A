97687511b91b5a0b70dc51c84888ea816669bab1
Luke O'Sullivan <osulluke@gmail.com>
2016-12-01 15:51:57 -0700

    Assignment 2, problem 5.
    Solution implements the max/min problem.

--- Assignment2/FindRange.java
+++ Assignment2/FindRange.java
@@ -1,7 +1,7 @@
 /*
  * File: FindRange.java
- * Name: 
- * Section Leader: 
+ * Name:
+ * Section Leader:
  * --------------------
  * This file is the starter file for the FindRange problem.
  */
@@ -9,8 +9,53 @@
 import acm.program.*;
 
 public class FindRange extends ConsoleProgram {
-	public void run() {
-		/* You fill this in */
-	}
-}
 
+	public void run() {
+		int num = 0;
+		
+		println("Enter numbers: ");
+		while(SENTINAL != 0) {
+			print("? ");
+			num = readInt();
+
+			if ((num == max) && (max == min) && (min == 0)) {
+				SENTINAL = 0;
+				println("You entered the sentinal on the first try!");
+				break;
+			}
+
+			else if (num == 0) {
+				SENTINAL = 0;
+				break;
+			}
+
+			else if ((max == min) && (min == 0)) {
+				max = num;
+				min = num;
+			}
+
+			else if (num <= min){
+				min = num;
+			}
+
+			else if (num >= max) {
+				max = num;
+			}
+		}
+
+		printNumbers();
+
+		return;
+	}
+
+	public void printNumbers() {
+		println("smallest: " + min);
+		println("largest: " + max);
+
+		return;
+	}
+
+	private int max = 0;
+	private int min = 0;
+	private int SENTINAL = 1;
+}
