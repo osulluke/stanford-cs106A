69f891cc68ec6c09e69dc6988035bb3750bdf48f
Luke O'Sullivan <osulluke@gmail.com>
2016-12-01 15:17:11 -0700

    Assignment 2, problem 4.
    This assignment makes use of some control/sentinal values to exit a loop. It wasn't
    working for a while, but a simple change of moving the sentinal value outside of the
    main run() function was all it took.

--- Assignment2/PythagoreanTheorem.java
+++ Assignment2/PythagoreanTheorem.java
@@ -9,7 +9,36 @@
 import acm.program.*;
 
 public class PythagoreanTheorem extends ConsoleProgram {
+	private int cont = 1;
+	
 	public void run() {
-		/* You fill this in */
+		while(cont > 0) {
+			pyth();
+		}
+		exit();
+		return;
+	}
+	
+	private void pyth() {
+		println("Enter values to compute Pythagorean Theorem:");
+		
+		print("a: ");
+		int a = readInt();
+		if(a <= 0) {
+			this.cont = -1;
+			return;
+		}
+		
+		print("b: ");
+		int b = readInt();
+		if(b <= 0) {
+			this.cont = -1;
+			return;
+		}
+		
+		double c = Math.sqrt(a*a + b*b);
+		println("c: " + c);
+		
+		return;
 	}
 }
