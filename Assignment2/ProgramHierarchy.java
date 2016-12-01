cc41ea8c1c731cbf2007b9868d0226c82df5c7bc
Luke O'Sullivan <osulluke@gmail.com>
2016-12-01 14:49:46 -0700

    Assignment 2, Problem 3.
    Completes the problem (though cheaply), but not really IAW the problem statement.

--- Assignment2/ProgramHierarchy.java
+++ Assignment2/ProgramHierarchy.java
@@ -1,7 +1,7 @@
 /*
  * File: ProgramHierarchy.java
- * Name: 
- * Section Leader: 
+ * Name:
+ * Section Leader:
  * ---------------------------
  * This file is the starter file for the ProgramHierarchy problem.
  */
@@ -10,9 +10,49 @@ import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
 
-public class ProgramHierarchy extends GraphicsProgram {	
+public class ProgramHierarchy extends GraphicsProgram {
 	public void run() {
-		/* You fill this in. */
+		/*
+		 * Add initial Program box.
+		 */
+		makeBox("Program", 350, 50);
+
+		/*
+		 * Add next row of "sub" program boxes.
+		 */
+		makeBox("GraphicsProgram", 150, 100);
+		makeBox("ConsoleProgram", 325, 100);
+		makeBox("DialogProgram", 500, 100);
+
+		/*
+		 * Build the three connecting lines.
+		 */
+		GLine line1 = new GLine(375, 58, 375, 85);
+		GLine line2 = new GLine(375, 58, 200, 85);
+		GLine line3 = new GLine(375, 58, 550, 85);
+
+		/*
+		 * Add the three connecting lines.
+		 */
+		add(line1);
+		add(line2);
+		add(line3);
+
+		return;
+	}
+	/*
+	 * This function makes a rectangle with a centered label, at the specified position.
+	 */
+	public void makeBox(String title, int x, int y) {
+		GLabel lab = new GLabel(title, x, y);
+
+		final int width = (int) lab.getWidth();
+		final int height = (int) lab.getHeight();
+
+		GRect rec = new GRect((x - width / 4), y - height, 1.5*width, 1.5*height);
+		add(lab);
+		add(rec);
+
+		return;
 	}
 }
-
