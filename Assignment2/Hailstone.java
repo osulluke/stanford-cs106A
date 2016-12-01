5e292d0349cb3c40e663c17bb9516f090e7bcacd
Luke O'Sullivan <osulluke@gmail.com>
2016-12-01 16:04:14 -0700

    Assignment 2, problem 6.
    Cool problem (and easy one) from the Godel, Escher, Bach world of Doug H.

--- Assignment2/Hailstone.java
+++ Assignment2/Hailstone.java
@@ -1,7 +1,7 @@
 /*
  * File: Hailstone.java
- * Name: 
- * Section Leader: 
+ * Name:
+ * Section Leader:
  * --------------------
  * This file is the starter file for the Hailstone problem.
  */
@@ -10,7 +10,21 @@ import acm.program.*;
 
 public class Hailstone extends ConsoleProgram {
 	public void run() {
-		/* You fill this in */
-	}
-}
+		print("Enter a number: ");
+		num = readInt();
 
+		while(num != 1) {
+			if (num % 2 == 0) {
+				println(num + " is even so I take half: " + (num = num / 2));
+			}
+			else {
+				println(num + " is odd, so I make 3n+1: " + (num = num * 3 + 1));
+			}
+			count += 1;
+		}
+		println("The process took " + count + " to reach 1");
+	}
+
+	private int num;
+	private int count = 0;
+}
