0b536338edf5c7b4485a6cc6159c4b7874e3ce73
Luke O'Sullivan <osulluke@gmail.com>
2016-12-04 13:19:35 -0700

    RandomCircles enhancements.
    Still not quite working the way I want it (i.e. keeping each of the circles positioned correctly within the bounds of the canvas).

--- Assignment3/RandomCircles.java
+++ Assignment3/RandomCircles.java
@@ -3,7 +3,7 @@ import acm.program.*;
 import acm.util.*;
 
 public class RandomCircles extends GraphicsProgram {
-	int numCircles = 10;
+	int numCircles = 20;
 	GOval[] circle = new GOval[numCircles];
 	int circleSize = 0;
 	int x = 0;
@@ -43,18 +43,76 @@ public class RandomCircles extends GraphicsProgram {
 		}
   		
   		while(true) {
+
   			for (int i = 0; i < numCircles; i++) {
   				circle[i].setColor(rgen.nextColor());
   				circle[i].setFillColor(circle[i].getColor());
   				pause(rgen.nextDouble(150, 400));
+  				
+  				/*
+  				 * Choose the positions within the bounds of the array
+  				 */
+
+  				
+  				int posX = rgen.nextInt(getWidth());
+  				int posY = rgen.nextInt(getHeight());
+  				
+  				if (circle[i].getX() < posX + circle[i].getWidth()) {
+  					posX = posX - (int) circle[i].getWidth();
+  				}
+  				else if (posX - circle[i].getX() > circle[i].getWidth()) {
+  					posX = posX + (int) circle[i].getWidth();
+  				}
+  				
+  				if (circle[i].getY() > posY + circle[i].getHeight()) {
+  					posY = posY - (int) 1.0 * (int) circle[i].getHeight();
+  				}
+  				else if (posY - circle[i].getY() > circle[i].getHeight()) {
+  					posY = posY + (int) 1.0 * (int) circle[i].getHeight();
+  				}
+  				/*
+  				 * Place the circle in the corrected area.
+  				 */
+   				circle[i].setLocation(posX, posY);
   			}
   			for (int i = 0; i < numCircles; i++) {
   				circle[i].setColor(rgen.nextColor());
   				circle[i].setFillColor(circle[i].getColor());
+  				/*
+  				 * Choose the positions within the bounds of the array
+  				 */
+  	  			int posX = 0;
+  	  			int posY = 0;
+  				
+  				posX = rgen.nextInt(getWidth());
+  				posY = rgen.nextInt(getHeight());
+  				
+  				if (circle[i].getX() < posX + circle[i].getWidth()) {
+  					posX = posX - (int) circle[i].getWidth();
+  				}
+  				else if (posX - circle[i].getX() > circle[i].getWidth()) {
+  					posX = posX + (int) circle[i].getWidth();
+  				}
+  				
+  				if (circle[i].getY() > posY + circle[i].getHeight()) {
+  					posY = posY - (int) circle[i].getHeight();
+  				}
+  				else if (posY - circle[i].getY() > circle[i].getHeight()) {
+  					posY = posY + (int) circle[i].getHeight();
+  				}
+  				/*
+  				 * Place the circle in the corrected area.
+  				 */
+  				circle[i].setLocation(posX, posY);
+  				
   			}
 			pause(rgen.nextDouble(150, 1000));
   		}
 	}
 	
+	private int fixX (int x) {
+		
+	}
+
 	private RandomGenerator rgen = RandomGenerator.getInstance();
 }
