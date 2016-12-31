00e409808011da1d82314e4cf92c78118df11010
Luke O'Sullivan <osulluke@gmail.com>
2016-12-30 20:13:54 -0700

    Initial Hangman commit.
    Code is working and initiailizing a new Lexicon, as well as displaying a welcome message and some basic game rules.

--- Assignment4/Hangman.java
+++ Assignment4/Hangman.java
@@ -14,7 +14,35 @@ import java.awt.*;
 public class Hangman extends ConsoleProgram {
 
     public void run() {
-		/* You fill this in */
+      /*Display welcome message, and initialize new wordlist.
+      */
+      welcomeToHangman();
+		/* Part 1 - playing an interactive game
+     */
+
+    /* Part 2 - implementing the graphics
+     */
+
+    /* Part 3 - reading in a new list of many words
+    */
 	}
 
+  public void welcomeToHangman() {
+    println("Welcome to Hangman!\nThe rules are just what you think!");
+    wordList = new HangmanLexicon();
+
+    return;
+  }
+
+  public char getNewGuess() {
+
+    return 'a';
+  }
+
+  /* Private (game) instance variables:
+    Includes:
+    - wordList: contains all words for the hangman game (not yet needed).
+    */
+  //private static String[] wordList = new String[10];
+  private HangmanLexicon wordList = null;
 }
