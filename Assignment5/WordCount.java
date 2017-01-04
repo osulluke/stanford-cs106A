c8129ad94f6b6ede2f2bc41933c08e2f8a5b25d9
Luke O'Sullivan <osulluke@gmail.com>
2017-01-03 17:41:41 -0700

    Finish moving Assignment5.

---
+++ Assignment5/WordCount.java
@@ -0,0 +1,32 @@
+import acm.util.*;
+import acm.io.*;
+
+public class WordCount extends ConsoleProgram {
+
+	public void run() {
+
+		BufferedReader rd = new BufferedReader(new FileReader("lear.txt"));
+
+		while(rd.)
+
+		return;
+	}
+
+	public int countChars() {
+		int numChars = 0;
+
+		return numChars;
+	}
+
+	public int countWords() {
+		int numWords = 0;
+
+		return numWords;
+	}
+
+	public int count
+
+	public int characterCount = 0;
+	public int wordCount = 0;
+	public int lineCount = 0;
+}
